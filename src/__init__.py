dd24a4be521d4dcea96fba0ed0cf86a7f57b64c2
Robert Portelli <github@robertportelli.com>
2024-03-26 13:09:50 -0400

    enable type checking

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-This is the main module of the `src` package. It contains the core functionality
-and classes for our Python application.
-
-This package is responsible for handling the application`s main logic and features.
-"""
