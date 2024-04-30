09e225774ce7e3b99f1e0512850ed7e0b7c56a29
Robert Portelli <github@robertportelli.com>
2024-04-30 17:03:43 -0400

    encapsulate node order reversal

--- src/ofnodes/structures/singlylinkedlist.py
+++ src/ofnodes/structures/singlylinkedlist.py
@@ -74,35 +74,39 @@ class SinglyLinkedList(SearchMixin, RemoveMixin, InsertHeadMixin, InsertTailMixi
         return sorted(parent_dir)
 
     def bubble_sort(self, ascending=True):
-        """Sorts the elements of the singly linked data structure in ascending order using the bubble sort algorithm.
+        """Sorts the elements of the singly linked data structure.
 
-        This method iteratively traverses the list and swaps adjacent elements if they are in the wrong order,
-        until the list is sorted.
+        Args:
+            ascending (bool, optional): Specifies whether to sort the elements in ascending order (default) or descending order.
 
-        Example:
+        Returns:
+            None: This method modifies the original linked list in place.
+
+        Examples:
             >>> sllist = SinglyLinkedList([8, 2, 6, 4, 5])
             >>> sllist.bubble_sort()
             >>> sllist
             SinglyLinkedList([2, 4, 5, 6, 8])
-            >>> str(sllist)
-            '2 -> 4 -> 5 -> 6 -> 8'
+            >>> sllist.bubble_sort(ascending=False)
+            >>> sllist
+            SinglyLinkedList([8, 6, 5, 4, 2])
 
-        Note:
-            - This method modifies the original linked list in place.
+        Notes:
             - The comparison operation (`>`) is used for elements. For non-numeric data types,
             ensure that the `__gt__` method is defined appropriately for comparison.
-
-        Time Complexity:
-            - Best Case: O(n), when the list is already sorted.
-            - Worst Case: O(n^2), when the list is in reverse order.
-            - Average Case: O(n^2).
+            - Time Complexity:
+                - Best Case: O(n), when the list is already sorted.
+                - Worst Case: O(n^2), when the list is in reverse order.
+                - Average Case: O(n^2).
         """
-
+        if not self.head or not self.head.next:  # it's a zero node or one node list
+            return
         # Flag to indicate whether any swaps were made in the current pass
         swapped = True
         while swapped:
             swapped = False
             # Start from the head of the list
+            # Complete ascending sort
             current = self._head
             while current and current.next:  # Add condition to check current.next
                 if current._data > current.next._data:
@@ -112,28 +116,35 @@ class SinglyLinkedList(SearchMixin, RemoveMixin, InsertHeadMixin, InsertTailMixi
                     swapped = True
                 # Move to the next node
                 current = current.next
-        if not ascending:
-            self.target = self.head
-            head_to_tail = self.head
-            self.head = SinglyNode(self.tail.data)
-            self.remove_tail()
-            self.head + SinglyNode(self.tail.data)
+        if not ascending:  # it's descending
+            self.reverse_order()
 
-    def insert_after_spot(self, spot: SinglyNode, node: SinglyNode):
+    def reverse_order(self):
+        """Reverses the order of elements in the singly linked data structure.
+
+        Returns:
+            None: This method modifies the original linked list in place.
+
+        Examples:
+            >>> sllist = SinglyLinkedList([8, 2, 6, 4, 5])
+            >>> sllist.reverse_order()
+            >>> sllist
+            SinglyLinkedList([5, 4, 6, 2, 8])
+
+        Notes:
+            - Time Complexity: O(n), where n is the number of elements in the linked list.
         """
-        >>> sllist = SinglyLinkedList([2, 4, 5, 6, 8])
-        >>> head_to_tail = sllist.head
-        >>> current = sllist.head
-        >>> #self.insert_after_spot(spot, node)
-        >>> sllist.insert_after_spot(current, SinglyNode(sllist.tail.data))
-        >>> sllist.remove_tail()
-        >>> sllist = SinglyLinkedList([8, 2, 4, 5, 6])
-        >>> assert head_to_tail is sllist.head.next
-        >>> current = current.next
-        >>> sllist.insert_after_spot(current, SinglyNode(sllist.tail.data))
-        >>> sllist.remove_tail()
-        >>> sllist = SinglyLinkedList([8, 6, 2, 4, 5]) TODO FIX
-        """
+        # set the head
+        head_to_tail = self._head  # this gets pushed to tail, triggers while loop exit
+        self._head = SinglyNode(self._tail.data)  # set the head with new node made from tail data(push old head one)
+        self.remove_tail()  # remove the node you moved data from
+        # complete node order reversal
+        current = self._head
+        while head_to_tail is not self._tail:  # or while head_to_tail.next is not None
+            node = SinglyNode(self._tail.data)
+            node._next, current._next = current._next, node
+            self.remove_tail()
+            current = current.next
 
 
 
