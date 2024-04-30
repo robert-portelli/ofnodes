f907e22df76662ab75de56003c047485447ec5a1
Robert Portelli <github@robertportelli.com>
2024-04-30 17:32:25 -0400

    support bubble sort and linked structure node reversal

--- tests/test_structures/test_singlylinkedlist.py
+++ tests/test_structures/test_singlylinkedlist.py
@@ -368,7 +368,31 @@ def test_cycle_detection():
     assert sllist.cycle_detection() is True
 
 def test_bubble_sort():
+    sllist = SinglyLinkedList()
+    with pytest.raises(ValueError) as exc_info:
+        sllist.bubble_sort()
+    assert "empty linked list" in str(exc_info)
+    def ascending():
+        sllist = SinglyLinkedList([8, 2, 6, 4, 5])
+        sllist.bubble_sort()
+        assert repr(sllist) == 'SinglyLinkedList([2, 4, 5, 6, 8])'
+        assert str(sllist) == '2 -> 4 -> 5 -> 6 -> 8'
+
+    def descending():
+        sllist = SinglyLinkedList([8, 2, 6, 4, 5])
+        sllist.bubble_sort(ascending=False)
+        assert repr(sllist) == 'SinglyLinkedList([8, 6, 5, 4, 2])'
+        assert str(sllist) == '8 -> 6 -> 5 -> 4 -> 2'
+
+    ascending()
+    descending()
+
+def test_reverse_order():
+    sllist = SinglyLinkedList()
+    with pytest.raises(ValueError) as exc_info:
+        sllist.reverse_order()
+    assert "empty linked list" in str(exc_info)
     sllist = SinglyLinkedList([8, 2, 6, 4, 5])
-    sllist.bubble_sort()
-    assert repr(sllist) == 'SinglyLinkedList([2, 4, 5, 6, 8])'
-    assert str(sllist) == '2 -> 4 -> 5 -> 6 -> 8'
+    sllist.reverse_order()
+    assert repr(sllist) == 'SinglyLinkedList([5, 4, 6, 2, 8])'
+    assert str(sllist) == '5 -> 4 -> 6 -> 2 -> 8'
