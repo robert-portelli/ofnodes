a49354c6efb9d9413923a3ff47dc1a7ab4e6e0ed
Robert Portelli <github@robertportelli.com>
2024-05-08 11:31:09 -0400

    prototype insertion sort

--- src/ofnodes/structures/randomaccessarray.py
+++ src/ofnodes/structures/randomaccessarray.py
@@ -15,6 +15,20 @@ class RandomAccessArray(BubbleSortMixin, ReverseOrderMixin):
     Note:
         This class inherits from BubbleSortMixin and ReverseOrderMixin to leverage the
         bubble sort and order reversal functionalities.
+
+    Examples:
+        >>> raarray = RandomAccessArray(5)
+        >>> raarray
+        RandomAccessArray([None, None, None, None, None])
+        >>> raarray[0] = 8
+        >>> raarray
+        RandomAccessArray([8, None, None, None, None])
+        >>> [raarray.__setitem__(i+1, val) for i, val in enumerate([2, 6, 4, 5])]
+        [None, None, None, None]
+        >>> raarray
+        RandomAccessArray([8, 2, 6, 4, 5])
+        >>> str(raarray)
+        '[8, 2, 6, 4, 5]'
     """
     def __init__(self, size):
         self._data = [None] * size
@@ -91,3 +105,38 @@ class RandomAccessArray(BubbleSortMixin, ReverseOrderMixin):
             >>> raarray
             RandomAccessArray([5, 4, 6, 2, 8])"""
         return super().index_based_reverse_order()
+
+    def index_based_insertion_sort(self):
+        """Sorts the elements of the data structure using the insertion sort algorithm with index-based access.
+
+        This method is suitable for short lists or lists that are mostly sorted.
+
+        Raises:
+            TypeError: If the data structure does not support index-based access.
+
+        Examples:
+            >>> raarray = RandomAccessArray([2, 4, 5, 6, 8])
+            >>> raarray
+            RandomAccessArray([2, 4, 5, 6, 8])
+
+            >>> raarray = RandomAccessArray(5)
+            >>> [raarray.__setitem__(i, val) for i, val in enumerate([8, 2, 6, 4, 5])]
+            [None, None, None, None, None]
+
+            >>> raarray.index_based_insertion_sort()
+            >>> raarray
+            RandomAccessArray([2, 4, 5, 6, 8])
+
+        Notes:
+            - The method modifies the original data structure in place.
+            - Time Complexity: O(n^2), where n is the number of elements in the data structure.
+        """
+        if '__getitem__' not in dir(self):
+            raise TypeError("index_based_insertion_sort can only be used on data structures that support index-based access.")
+        for unsorted in range(1, len(self)):
+            value = self[unsorted]  # persist the value found using the first key, `i`.
+            j = unsorted - 1  # compute the first index of the sorted subarray
+            while j >= 0 and value < self[j]:
+                self[j + 1] = self[j]  # shift the value of the sorted subarray one to the right
+                j -= 1  # compute the next index of the sorted subarray
+            self[j + 1] = value  # insert the value one to the right of the minimum value
